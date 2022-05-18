983aded2c15910f7d1a87ee286f2978a6f87165e
Alex <youzhny.foto@gmail.com>
2022-05-18 17:54:03 +0300

    exercise_11 - done!

--- js/exerc_3.js
+++ js/exerc_3.js
@@ -333,3 +333,34 @@ function showProgrammingLangs(plan) {
 
 showProgrammingLangs(personalPlanPeter);
 
+
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+    
+    let str = '';
+
+    arr.length === 0 ? str = 'Семья пуста' : str = 'Семья состоит из: ';
+
+    arr.forEach(member => {
+        str += `${member} `
+    });
+    
+    return str;
+}
+
+showFamily(family);
+
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+
+    let str = '';
+
+    arr.forEach (function(city) {
+        console.log (city.toLowerCase())
+    });        
+}
+
+standardizeStrings(favoriteCities);
